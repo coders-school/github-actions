7ca2b7e1d76a4e54901100fe73b2968357c8c10f
Andrzej Rafalski <anraf1001@gmail.com>
2022-06-22 16:52:30 +0200

    add excluded dir parameter

--- clang-format-check/clang-format-check.sh
+++ clang-format-check/clang-format-check.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-find "$1" -regex '.*\.\(cpp\|hpp\|cc\|cxx\|h\)$' -not -regex ".*catch.*" | xargs -P$(nproc) -n2 clang-format -style=file -i
+if [[ $# -ge 2 ]]; then
+    find "$1" -regex '.*\.\(cpp\|hpp\|cc\|cxx\|h\)$' -not -path "$2/*" | xargs -P$(nproc) -n2 clang-format -style=file -i
+else
+    find "$1" -regex '.*\.\(cpp\|hpp\|cc\|cxx\|h\)$' | xargs -P$(nproc) -n2 clang-format -style=file -i
+fi
 
 CHANGES=$(git --no-pager diff)
 if [[ -n "$CHANGES" ]]; then
